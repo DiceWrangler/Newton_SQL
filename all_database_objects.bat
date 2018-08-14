d737bfb2d70ae5571c372f414fee4a73bf95b857
Scott Thorne <dicewrangler@gmail.com>
2018-08-14 12:23:36 -0600

    change dbname to "newton"

--- all_database_objects.bat
+++ all_database_objects.bat
@@ -7,7 +7,7 @@ type func_*.sql > all_functions.sql
 type view_*.sql > all_views.sql
 type index_*.sql > all_indices.sql
 
-psql -h localhost -p 5432 -U postgres -d dogstar -a < all_tables.sql > all_tables.log
-psql -h localhost -p 5432 -U postgres -d dogstar -a < all_functions.sql > all_functions.log
-psql -h localhost -p 5432 -U postgres -d dogstar -a < all_views.sql > all_views.log
-psql -h localhost -p 5432 -U postgres -d dogstar -a < all_indices.sql > all_indices.log
+psql -h localhost -p 5432 -U postgres -d newton -a < all_tables.sql > all_tables.log
+psql -h localhost -p 5432 -U postgres -d newton -a < all_functions.sql > all_functions.log
+psql -h localhost -p 5432 -U postgres -d newton -a < all_views.sql > all_views.log
+psql -h localhost -p 5432 -U postgres -d newton -a < all_indices.sql > all_indices.log
